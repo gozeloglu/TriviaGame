5ec16348721ccb3ac2b70a1514ce6f84b5ce80ef
gozeloglu <gozeloglu@gmail.com>
2020-03-05 17:03:04 +0300

    newGame is added

--- trivia_game.py
+++ trivia_game.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
+from random import randint
+
+prev_games = []
 
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -17,15 +20,34 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(str.encode(mes+"\n"))
 
+    def new_game(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(str.encode("New Trivia Game started\nSession ID = "+ str(prev_games[-1]) + "\n"))
+        print("New Game Session ID: ", prev_games[-1])  # debug print
+
     def do_GET(self):
         # Parse incoming request url
         url = urlparse(self.path)
+        print("\n", url, "\n")  # debug print
         if url.path == "/hello":
             return self.say_hello(parse_qs(url.query))
-        # return 404 code if path not found
-        self.send_response(404)
-        self.end_headers()
-        self.wfile.write(b'Not Found!\n')
+        elif url.path == "/newGame":
+            self.create_session_id()
+            self.new_game()
+            #return self.say_hello(parse_qs(url.query))
+        else:
+            # return 404 code if path not found
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'Not Found!\n')
+
+    def create_session_id(self):
+        new_id = randint(1, 100)
+        if new_id in prev_games:
+            self.create_session_id()
+        else:
+            prev_games.append(new_id)
 
 
 if __name__ == "__main__":
@@ -33,3 +55,4 @@ if __name__ == "__main__":
     print(f'Listening on localhost:{port}')
     server = HTTPServer(('', port), RequestHandler)
     server.serve_forever()
+    server.server_close()
